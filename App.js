6ecf67347b18b04732175dfa5ebc596bd133e17e
Taufik Nurrohman <onlytaufik47@gmail.com>
2023-11-08 19:45:43 +0700

    menambahkan halaman login

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text, StyleSheet, Image, Pressable } from 'react-native';
+import { View, Text, StyleSheet, Image, Pressable, TextInput} from 'react-native';
 import {LinearGradient} from 'expo-linear-gradient'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -43,15 +43,28 @@ function DetailScreen({ navigation }) {
   );
 }
 
-function LoginScreen() {
+function LoginScreen({ navigation }) {
+  const handleNavigation = () => {
+    navigation.navigate('Detail');
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'#202326'}}>
-      <Image
-        style={styles.image}
-        source={require('./assets/first.png')}
-      />
-      <Text style={styles.judulKiri}>Login</Text>
-      <Text style={styles.paragraph}>Tulis semua catatanmu dengan mudah dan sederhana hanya disini!</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#202326' }}>
+      <Text style={styles.judulKiri}>Siap Menjadi Produktif? Daftar</Text>
+      <Text style={styles.textKiri} marginTop={30}>Nama</Text>
+      <TextInput style={styles.input} placeholder="Masukkan Nama" placeholderTextColor="grey"/>
+      <Text style={styles.textKiri}>Email</Text>
+      <TextInput style={styles.input} placeholder="Masukkan Email" placeholderTextColor="grey"/>
+      <Text style={styles.textKiri}>Kata Sandi</Text>
+      <TextInput style={styles.input} placeholder="Masukkan Kata Sandi" secureTextEntry={true} placeholderTextColor="grey" marginBottom= {150} />
+      <Pressable
+        style={styles.button}
+        onPress={() => navigation.navigate('Login')}>
+        <Text style={styles.text}>  Daftar  </Text>
+        </Pressable>
+      <Text style={styles.text} marginTop={20} marginBottom={-50}>Sudah punya akun?
+        <Text style={styles.masuk} onPress={handleNavigation}>Masuk</Text>
+      </Text>
     </View>
   );
 }
@@ -80,6 +93,15 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 50,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: 320,
+    borderRadius: 5,
+    paddingLeft: 10,
+    marginBottom: 30,
   },
   judulTengah: {
     fontWeight: 'bold',
@@ -115,7 +137,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 32,
     borderRadius: 15,
     elevation: 3,
-    backgroundColor: '#1C9DBE',
+    backgroundColor: '#007DFF',
   },
   text:{
     color: 'white',
@@ -123,6 +145,20 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     paddingHorizontal: 100,
     paddingVertical: 8,
+  },
+  masuk:{
+    color: '#007DFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+  },
+  textKiri:{
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: 'white',
+    width: 320,
+    marginBottom: 10,
   }
   }
   
