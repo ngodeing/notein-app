566a37192a674f97697e1da46e8fc3d89d92fa3b
Taufik Nurrohman <onlytaufik47@gmail.com>
2023-11-24 18:48:18 +0700

    Menambahkan Bagian Reminder

--- screens/EditTextScreen.js
+++ screens/EditTextScreen.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { View, Image, Pressable, TextInput, ScrollView} from 'react-native';
+import { View, Image, Pressable, TextInput, ScrollView, Modal, Text, StyleSheet} from 'react-native';
 
 
 export default function EditTextScreen({navigation, route,notes}) {
     const [text, setText] = useState('');
     const [textjudul, setTextjudul] = useState('');
     const { onNoteSaved, initialNoteData } = route.params;
+    const [confirmationModalVisible, setConfirmationModalVisible] = useState(false);
   
     useEffect(() => {
       // Set initial values when the component mounts
@@ -27,6 +28,10 @@ export default function EditTextScreen({navigation, route,notes}) {
   
       navigation.goBack();
     };
+
+    const showDateReminder = () => {
+      setConfirmationModalVisible(true);
+    };
   
     return (
       <ScrollView backgroundColor= '#202326'>
@@ -38,6 +43,11 @@ export default function EditTextScreen({navigation, route,notes}) {
             <Image
           style={{width: 20, height: 20, marginLeft: 20, justifyContent: 'flex-start'}}
           source={require('./../assets/images/back.png')}
+        /></Pressable>
+        <Pressable onPress={showDateReminder}>
+            <Image
+          style={{width: 20, height: 20, marginLeft: 10, justifyContent: 'flex-center'}}
+          source={require('./../assets/images/calendar.png')}
         /></Pressable>
         <Pressable onPress={handleSave}>
         <Image
@@ -79,6 +89,254 @@ export default function EditTextScreen({navigation, route,notes}) {
           value={text}
           onChangeText={setText}
         />
-      
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={confirmationModalVisible}
+      onRequestClose={() => setConfirmationModalVisible(false)}
+    >
+      <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              padding: 20,
+              paddingTop: 250,
+              backgroundColor: '#202326',
+              alignItems: 'center',
+            }}
+          >
+            <View>
+              <Text
+                style={{
+                  color: 'white',
+                  fontSize: 20,
+                  fontWeight: 'bold',
+                  marginBottom: 20,
+                  textAlign: 'center',
+                }}
+              >
+                Apakah catatan ini ingin dihapus?
+              </Text>
+              <View
+                style={{
+                  flex: 1,
+                  flexDirection: 'row-reverse',
+                  justifyContent: 'center',
+                  gap: 20,
+                }}
+              >
+                <Pressable style={styles.addButtonY}>
+                  <Text style={{ color: 'white' }}>Ya</Text>
+                </Pressable>
+                <Pressable
+                  onPress={() => setConfirmationModalVisible(false)}
+                  style={styles.addButtonS}
+                >
+                  <Text style={{ color: 'white' }}>Tidak</Text>
+                </Pressable>
+              </View>
+            </View>
+          </View>
+    </Modal>
       </ScrollView>
     )}
+
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      imageKecil:{
+        width: 70,
+        height: 80,
+        marginBottom: 20,
+      },
+      progressContainer: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        marginRight: 10,
+        alignItems: 'center',
+      },
+      circle: {
+        width: 110,
+        height: 110,
+        borderRadius: 55,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      circleText: {
+        color: 'white',
+        fontSize: 30,
+        fontWeight: 'bold',
+      },
+      input: {
+        height: 50,
+        color: 'white',
+        borderColor: 'gray',
+        borderWidth: 1,
+        width: 320,
+        borderRadius: 5,
+        paddingLeft: 10,
+        marginBottom: 30,
+      },
+      judulTengah: {
+        fontWeight: 'bold',
+        fontSize: 40,
+        color: 'white',
+        width: 320,
+        textAlign: 'center',
+        marginBottom: 20,
+      },
+      judulKiri: {
+        fontWeight: 'bold',
+        fontSize: 40,
+        color: 'white',
+        width: 320,
+        textAlign: 'left',
+        marginBottom: 30,
+      },
+      judul2Kiri: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: 'white',
+        width: 320,
+        textAlign: 'left',
+        marginBottom: 30,
+      },
+      paragraph: {
+        fontWeight: 'medium',
+        fontSize: 16,
+        color: 'gray',
+        width: 300,
+      },
+      paragraphCP: {
+        fontWeight: 'medium',
+        fontSize: 16,
+        color: 'gray',
+        width: 170,
+      },
+      paragraphCD: {
+        fontWeight: 'medium',
+        fontSize: 15,
+        color: 'gray',
+        width: 80,
+      },
+      image:{
+        marginTop: 0,
+        marginBottom: 70,
+        width: 300,
+        height: 300,
+      },
+      button: {
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 15,
+        elevation: 3,
+        backgroundColor: '#007DFF',
+      },
+      cardPanjang: {
+        paddingVertical: 25,
+        paddingHorizontal: 25,
+        marginBottom: 30,
+        borderRadius: 25,
+        width: 340,
+        backgroundColor: '#2F3235',
+      },
+      card: {
+        paddingVertical: 15,
+        gap: 5,
+        paddingHorizontal: 20,
+        marginBottom: 25,
+        borderRadius: 15,
+        width: 340,
+        backgroundColor: '#2F3235',
+      },
+      cardPendek: {
+        paddingVertical: 15,
+        paddingHorizontal: 25,
+        borderRadius: 15,
+        width: 160,
+        backgroundColor: '#2F3235',
+      },
+      text:{
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+        paddingHorizontal: 100,
+        paddingVertical: 8,
+      },
+      text2:{
+        color: 'white',
+        fontSize: 16,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        width: 320,
+      },
+      textCP:{
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold',
+        width: 170,
+        marginBottom: 10,
+      },
+      textCD:{
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
+        width: 80,
+      },
+      masuk:{
+        color: '#007DFF',
+        fontSize: 16,
+        fontWeight: 'bold',
+        paddingHorizontal: 10,
+        paddingVertical: 8,
+      },
+      textKiri:{
+        fontWeight: 'bold',
+        fontSize: 16,
+        color: 'white',
+        width: 300,
+        marginBottom: 10,
+      },
+      addButtonContainer: {
+        position: 'absolute',
+        bottom: 20,
+        right: 20,
+      },
+      addButton: {
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        backgroundColor: '#007DFF',
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 5,
+      },
+      addButtonS: {
+        width: 100,
+        height: 50,
+        borderRadius: 15,
+        backgroundColor: '#007DFF',
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 5,
+      },
+      addButtonText: {
+        color: 'white',
+        fontSize: 24,
+        fontWeight: 'bold',
+      },
+      addButtonY: {
+        width: 100,
+        height: 50,
+        borderRadius: 15,
+        backgroundColor: '#FE0000',
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 5,
+      },
+      }
+      
+    );
