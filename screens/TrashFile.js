756c4a50699da94213870d775682c6196bfaba3e
Taufik Nurrohman <onlytaufik47@gmail.com>
2023-11-29 18:06:21 +0700

    selesai membuat hapus banyak

--- screens/TrashFile.js
+++ screens/TrashFile.js
@@ -1,112 +1,264 @@
-import {Modal, View, Text, StyleSheet, Image, Pressable, ScrollView} from 'react-native';
-import { LongPressGestureHandler } from 'react-native-gesture-handler';
-import React, { useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Image, Pressable, ScrollView, Modal, TouchableOpacity } from 'react-native';
+import { LongPressGestureHandler, State } from 'react-native-gesture-handler';
+import CheckboxIcon from './../assets/images/checkbox.png';
+import CheckboxIconUnchecked from './../assets/images/CheckboxIconUnchecked.png';
+import TrashIcon from './../assets/images/t4sampahputih.png';
 
-
-export default function TrashFiles({ navigation, trash, setTrash}) {
+export default function TrashFiles({ navigation, trash, setTrash }) {
+  const [selectedTrash, setSelectedTrash] = useState([]);
   const [trashToDelete, setTrashToDelete] = useState(null);
   const [confirmationModalVisible, setConfirmationModalVisible] = useState(false);
-  
+  const [selectAllChecked, setSelectAllChecked] = useState(false);
+
+  useEffect(() => {
+    return () => setSelectedTrash([]);
+  }, [trash]);
+
   const deleteTrash = () => {
-    const updatedTrash = trash.filter((trash) => trash !== trashToDelete);
+    const updatedTrash = trash.filter((_, index) => !selectedTrash.includes(index));
     setTrash(updatedTrash);
-  
+
+    setSelectedTrash([]);
     setTrashToDelete(null);
+    setSelectAllChecked(false);
     setConfirmationModalVisible(false);
   };
-  
-  const showDeleteConfirmation = (trash) => {
-    setTrashToDelete(trash);
+
+  const toggleSelectTrash = (index) => {
+    setSelectedTrash((prevSelected) => {
+      if (prevSelected.includes(index)) {
+        return prevSelected.filter((selected) => selected !== index);
+      } else {
+        return [...prevSelected, index];
+      }
+    });
+  };
+
+  const handleSelectAll = () => {
+    setSelectAllChecked(!selectAllChecked);
+    setSelectedTrash(selectAllChecked ? [] : [...Array(trash.length).keys()]);
+  };
+
+  const handleUnselectAll = () => {
+    setSelectedTrash([]);
+    setSelectAllChecked(false);
+  };
+
+  const handleToggleSelectAll = () => {
+    setSelectAllChecked(!selectAllChecked);
+    setSelectedTrash(selectAllChecked ? [] : [...Array(trash.length).keys()]);
+  };
+
+  const showDeleteConfirmation = () => {
     setConfirmationModalVisible(true);
   };
-    return (
-      <View style={{ backgroundColor: '#202326', flex:1, }}>
+
+  return (
+    <View style={{ backgroundColor: '#202326', flex: 1 }}>
       <ScrollView>
-              <Pressable
-          style={{flex: 1, flexDirection: 'row', width: 370, justifyContent: 'space-between' , marginTop:50, marginBottom:50}}
-          onPress={() => navigation.navigate('Primary')}>
-            <Image
-          style={{width: 20, height: 20, marginLeft: 20, justifyContent: 'flex-start'}}
-          source={require('./../assets/images/back.png')}
-        />
-          </Pressable>
-        <View style={{ flex: 1, alignItems: 'center', backgroundColor: '#202326', paddingTop: 0 }}>
-          <Text style={styles.judulKiri}>Sampah</Text>
-          <View style={{flexDirection:'row', gap:20, flexWrap:"wrap", justifyContent:"center"}}>
-          {trash.map((trash, index) => (
-            <LongPressGestureHandler
-            key={index}
-            onHandlerStateChange={({ nativeEvent }) => {
-              if (nativeEvent.state === 4) {
-                showDeleteConfirmation(trash);
-              }
+        <Pressable
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            width: 370,
+            justifyContent: 'space-between',
+            marginTop: 50,
+            marginBottom: 50,
+          }}
+          onPress={() => navigation.navigate('Primary')}
+        >
+          <Image
+            style={{
+              width: 20,
+              height: 20,
+              marginLeft: 20,
+              justifyContent: 'flex-start',
             }}
-          >
-            <View
-              key={index}
-              style={styles.card}
-              marginBottom = {0}
-              >
-              <Text style={styles.textKiri} numberOfLines={1}>{trash.title}</Text>
-              <Text style={styles.paragraph} numberOfLines={2}>{trash.notes}</Text>
-            </View>
-            </LongPressGestureHandler>
-          ))}
-          <Modal
-          animationType="slide"
-          transparent={true}
-          visible={confirmationModalVisible}
-          onRequestClose={() => setConfirmationModalVisible(false)}
+            source={require('./../assets/images/back.png')}
+          />
+        </Pressable>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            backgroundColor: '#202326',
+            paddingTop: 0,
+          }}
         >
           <View
             style={{
-              flex: 1,
-              justifyContent: 'center',
-              padding: 20,
-              paddingTop: 250,
-              backgroundColor: '#202326',
+              flexDirection: 'row',
               alignItems: 'center',
+              marginBottom: 20,
             }}
           >
-            <View>
-              <Text
-                style={{
-                  color: 'white',
-                  fontSize: 20,
-                  fontWeight: 'bold',
-                  marginBottom: 20,
-                  textAlign: 'center',
-                }}
-              >
-                Apakah catatan ini ingin dihapus secara permanen?
-              </Text>
-              <View
-                style={{
-                  flex: 1,
-                  flexDirection: 'row-reverse',
-                  justifyContent: 'center',
-                  gap: 20,
-                }}
-              >
-                <Pressable onPress={deleteTrash} style={styles.addButtonY}>
-                  <Text style={{ color: 'white' }}>Ya</Text>
-                </Pressable>
-                <Pressable
-                  onPress={() => setConfirmationModalVisible(false)}
-                  style={styles.addButtonS}
-                >
-                  <Text style={{ color: 'white' }}>Tidak</Text>
-                </Pressable>
-              </View>
-            </View>
+            {selectedTrash.length === 0 ? (
+              <>
+                <Text style={styles.judulKiri}>Sampah</Text>
+                <TouchableOpacity onPress={handleSelectAll}>
+                  <Image
+                    style={{
+                      width: 30,
+                      height: 30,
+                      right: 7.5,
+                      position: 'absolute',
+                      marginTop: -25,
+                      tintColor: selectedTrash.length > 0 ? '#007DFF' : '#999999',
+                      zIndex: 1,
+                    }}
+                    source={selectAllChecked ? CheckboxIcon : CheckboxIconUnchecked}
+                  />
+                </TouchableOpacity>
+                {selectedTrash.length > 0 && (
+                  <Pressable onPress={handleUnselectAll}>
+                    <Text style={{ color: '#FE0000', marginLeft: 10 }}>Batal</Text>
+                  </Pressable>
+                )}
+              </>
+            ) : (
+              <>
+                <Text style={{ color: 'white', marginBottom: 5, marginEnd: 110 }}>Hapus semua catatan?</Text>
+                <TouchableOpacity onPress={handleToggleSelectAll}>
+                  <Image
+                    style={{
+                      width: 25,
+                      height: 25,
+                      marginRight: 20,
+                      tintColor: '#FE0000',
+                    }}
+                    source={selectAllChecked ? CheckboxIconUnchecked : CheckboxIcon}
+                  />
+                </TouchableOpacity>
+              </>
+            )}
+            {selectedTrash.length > 0 && (
+              <Pressable onPress={showDeleteConfirmation}>
+                <Image
+                  style={{
+                    width: 25,
+                    height: 25,
+                    tintColor: '#FE0000',
+                  }}
+                  source={TrashIcon}
+                />
+              </Pressable>
+            )}
           </View>
-        </Modal>
+          <View
+            style={{
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              justifyContent: 'center',
+              columnGap: 20,
+            }}
+          >
+            {trash.map((trashItem, index) => (
+              <LongPressGestureHandler
+                key={index}
+                onHandlerStateChange={({ nativeEvent }) => {
+                  if (nativeEvent.state === State.ACTIVE) {
+                    showDeleteConfirmation();
+                  }
+                }}
+              >
+                <View>
+                  <TouchableOpacity
+                    style={[styles.card, { borderColor: selectedTrash.includes(index) ? '#007DFF' : 'transparent' }]}
+                    onPress={() => toggleSelectTrash(index)}
+                  >
+                    <Text style={styles.textKiri} numberOfLines={1}>
+                      {trashItem.title}
+                    </Text>
+                    <Text style={styles.paragraph} numberOfLines={2}>
+                      {trashItem.notes}
+                    </Text>
+                    <TouchableOpacity
+                      style={{
+                        width: 30,
+                        height: 30,
+                        position: 'absolute',
+                        top: 5,
+                        right: 5,
+                      }}
+                      onPress={() => toggleSelectTrash(index)}
+                    >
+                      <Image
+                        style={{
+                          width: 30,
+                          height: 30,
+                          top: 25,
+                          right: 15,
+                          tintColor: '#999999',
+                        }}
+                        source={selectedTrash.includes(index) ? CheckboxIcon : CheckboxIconUnchecked}
+                      />
+                    </TouchableOpacity>
+                  </TouchableOpacity>
+                </View>
+              </LongPressGestureHandler>
+            )).reverse()}
           </View>
         </View>
       </ScrollView>
-      </View>
-    )
-  }
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={confirmationModalVisible}
+        onRequestClose={() => {
+          setConfirmationModalVisible(false);
+          setSelectedTrash([]);
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            padding: 20,
+            paddingTop: 250,
+            backgroundColor: '#202326',
+            alignItems: 'center',
+          }}
+        >
+          <View>
+            <Text
+              style={{
+                color: 'white',
+                fontSize: 20,
+                fontWeight: 'bold',
+                marginBottom: 20,
+                textAlign: 'center',
+              }}
+            >
+              Apakah catatan ini ingin dihapus secara permanen?
+            </Text>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'row-reverse',
+                justifyContent: 'center',
+                gap: 20,
+              }}
+            >
+              <Pressable onPress={deleteTrash} style={styles.addButtonY}>
+                <Text style={{ color: 'white' }}>Ya</Text>
+              </Pressable>
+              <Pressable
+                onPress={() => {
+                  setConfirmationModalVisible(false);
+                  setSelectedTrash([]);
+                }}
+                style={styles.addButtonS}
+              >
+                <Text style={{ color: 'white' }}>Tidak</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+}
 
   const styles = StyleSheet.create({
     container: {
